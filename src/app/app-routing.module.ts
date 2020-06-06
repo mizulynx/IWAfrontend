6cb73b72f37dc5533fbc63da51e027dfa570ff26
Dracon <Dracon@localhost.localdomain>
2020-06-06 16:43:50 +0200

    change navbar map doesn't work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
+import { LoginComponent } from './login/login.component';
 import { RulesComponent } from './rules/rules.component';
 import { DataComponent } from './data/data.component';
 import { PlanComponent } from './plan/plan.component';
-import { InfoComponent } from './info/info.component';
 import { ParticipantZoneComponent } from './participant-zone/participant-zone.component';
 import { StartComponent } from './start/start.component';
 import { NgModule, Component } from '@angular/core';
@@ -11,11 +11,10 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {path: '', redirectTo: 'start', pathMatch: 'full'},
   {path: 'start', component: StartComponent},
-  //{path: 'rules', component: ContactComponent},
   {path: 'plan', component: PlanComponent},
   {path: 'data', component: DataComponent},
   {path: 'rules', component: RulesComponent},
-  {path: 'participantzone', component: ParticipantZoneComponent}
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartComponent } from './start/start.component';
 import { ParticipantZoneComponent } from './participant-zone/participant-zone.component';
-import { InfoComponent } from './info/info.component';
 import { RulesComponent } from './rules/rules.component';
 import { PlanComponent } from './plan/plan.component';
 import { DataComponent } from './data/data.component';
-
+import {GoogleMapsModule} from '@angular/google-maps';
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
     StartComponent,
     ParticipantZoneComponent,
-    InfoComponent,
     RulesComponent,
     PlanComponent,
-    DataComponent
+    DataComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
